6c9cd55e153319e0a8de8b094f27e5bde6bfffc3
“Lindelanimcebo” <lindelanimcebo@gmail.com>
2020-03-15 01:11:56 +0200

    computed properties

--- main.js
+++ main.js
@@ -3,19 +3,20 @@ var app = new Vue({
     data : {
         product : "Socks",
         description : "I am just learning this thing",
-        image : "./assets/vmSocks-green-onWhite.jpg",
-        inStock : false,
+        selectedVariant : 0,
         details : ['80% cool', 'not too bad', 'etc'],
         variants : [
             {
                 variantId : 2234,
                 variantColor : 'green',
-                variantImage: './assets/vmSocks-green-onWhite.jpg'
+                variantImage: './assets/vmSocks-green-onWhite.jpg',
+                variantQuantity: 0
             },
             {
                 variantId : 2235,
                 variantColor : 'blue',
-                variantImage : './assets/vmSocks-blue-onWhite.jpg'
+                variantImage : './assets/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 10 
             }
         ],
         cart : 0
@@ -24,8 +25,16 @@ var app = new Vue({
         addToCart : function () {
             this.cart += 1;
         },
-        updateProduct(variantImage) {
-            this.image = variantImage;
+        updateProduct(index) {
+            this.selectedVariant= index;
+        }
+    },
+    computed : {
+        image () {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
         }
     }
 })
