2282913b33df083cd948c8b4573c32024e8d89c8
“Lindelanimcebo” <lindelanimcebo@gmail.com>
2020-03-14 23:33:58 +0200

    Event handling

--- main.js
+++ main.js
@@ -6,6 +6,26 @@ var app = new Vue({
         image : "./assets/vmSocks-green-onWhite.jpg",
         inStock : false,
         details : ['80% cool', 'not too bad', 'etc'],
-        sizes : ['ss', 's', 'm']
+        variants : [
+            {
+                variantId : 2234,
+                variantColor : 'green',
+                variantImage: './assets/vmSocks-green-onWhite.jpg'
+            },
+            {
+                variantId : 2235,
+                variantColor : 'blue',
+                variantImage : './assets/vmSocks-blue-onWhite.jpg'
+            }
+        ],
+        cart : 0
+    },
+    methods : {
+        addToCart : function () {
+            this.cart += 1;
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
+        }
     }
 })
