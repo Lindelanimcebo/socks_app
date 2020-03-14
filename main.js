59cbbd9b07c4a0d7f452d8fca20efbe3e5bcbb34
“Lindelanimcebo” <lindelanimcebo@gmail.com>
2020-03-14 22:41:57 +0200

    attributes binding

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var app = new Vue({
     el : "#app",
     data : {
-        product : "Boots",
-        description : "I am just learning this thing"
+        product : "Socks",
+        description : "I am just learning this thing",
+        image : "./assets/vmSocks-green-onWhite.jpg",
+        github : "https://github.com/Lindelanimcebo/socks_app"
     }
 })
